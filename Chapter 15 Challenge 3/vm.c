5f14f879e72169bf843d4391a71869097473428e
AvidAardvark <144076718+AvidAardvark@users.noreply.github.com>
2026-03-24 20:13:40 -0700

    Create vm.c

---
+++ Chapter 15 Challenge 3/vm.c
@@ -0,0 +1,20 @@
+void initVM() {
+  vm.stackCapacity = 256;
+   vm.stack = malloc(sizeof(Value) * vm.stackCapacity);
+  resetStack();
+}
+
+void freeVM() {
+  free(vm.stack);
+}
+
+void push(Value value) {
+  if (vm.stackTop - vm.stack >= vm.stackCapacity) {
+    vm.stackCapacity *= 2;
+    vm.stack = realloc(vm.stack, sizeof(Value) * vm.stackCapacity);
+    vm.stackTop = vm.stack + vm.stackCapacity / 2;
+  }
+
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
